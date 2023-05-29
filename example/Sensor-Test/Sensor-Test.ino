ed6d452f6a35a9b23d35708cbebbf1d9b7d3463b
一九五 <2259761058@qq.com>
2023-05-29 15:16:16 +0800

    Add sensor test demo

---
+++ example/Sensor-Test/Sensor-Test.ino
@@ -0,0 +1,156 @@
+// Vincent Fix 2023/5/24
+// Need Choice Sensor Type !
+
+#define SENSOR_5_PIN
+// #define SENSOR_3_PIN
+
+#include <HardwareSerial.h>
+
+#include "pin_config.h"
+
+HardwareSerial MySerial(1);
+
+unsigned char resp[80] = {0};
+
+int humidity = 0;
+int tem = 0;
+int ph = 0;
+float humidity_value = 0.0;
+float tem_value = 0.0;
+float ph_value = 0.0;
+int P_value = 0;
+int N_value = 0;
+int K_value = 0;
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(4800);
+    MySerial.begin(4800, SERIAL_8N1, 23, 22);
+
+    pinMode(Display_power, OUTPUT);
+    pinMode(RS485_power, OUTPUT);
+    delay(50);
+    digitalWrite(Display_power, HIGH);
+    digitalWrite(RS485_power, HIGH);
+    delay(1000);
+
+    Serial.println(" Test Begin!");
+}
+
+void loop()
+{
+    Serial.println("Send loop");
+
+#ifdef SENSOR_5_PIN
+    sensor_read_5pin();
+    value_log();
+    delay(3000);
+#endif
+
+#ifdef SENSOR_3_PIN
+    sensor_read_3pin();
+    value_log();
+    delay(3000);
+#endif
+}
+
+void sensor_read_3pin()
+{
+    unsigned char ask_cmd[8] = {0X01, 0X03, 0X00, 0X00, 0X00, 0X02, 0XC4, 0X0B};
+    MySerial.write(ask_cmd, 8);
+    int i = 0;
+
+    while (MySerial.available() > 0 && i < 80)
+    {
+        resp[i] = MySerial.read();
+        i++;
+
+        yield();
+    }
+    Serial.print("Answer Length:");
+    Serial.println(i);
+
+    for (int j = 0; j < 19; j++)
+    {
+        Serial.print((int)resp[j]);
+        Serial.print("  ");
+    }
+    Serial.print("\n");
+
+    humidity = CaculateValue((int)resp[3], (int)resp[4]);
+    humidity_value = humidity * 0.1;
+    tem = CaculateValue((int)resp[5], (int)resp[6]);
+    tem_value = tem * 0.1;
+}
+
+void sensor_read_5pin()
+{
+    unsigned char ask_cmd[8] = {0X01, 0X04, 0X00, 0X00, 0X00, 0X07, 0XB1, 0XC8};
+    MySerial.write(ask_cmd, 8);
+    int i = 0;
+
+    while (MySerial.available() > 0 && i < 80)
+    {
+        resp[i] = MySerial.read();
+        i++;
+
+        yield();
+    }
+
+    Serial.print("Answer Length:");
+    Serial.println(i);
+
+    char temp[20];
+    for (int j = 0; j < 19; j++)
+    {
+        sprintf(temp, "%02X ", (int)resp[j]);
+        Serial.printf(temp);
+    }
+
+    Serial.print("\n");
+
+    humidity = CaculateValue((int)resp[3], (int)resp[4]);
+    humidity_value = humidity * 0.1;
+    tem = CaculateValue((int)resp[5], (int)resp[6]);
+    tem_value = tem * 0.1;
+    ph = CaculateValue((int)resp[9], (int)resp[10]);
+    ph_value = ph * 0.1;
+    N_value = CaculateValue((int)resp[11], (int)resp[12]);
+    P_value = CaculateValue((int)resp[13], (int)resp[14]);
+    K_value = CaculateValue((int)resp[15], (int)resp[16]);
+}
+
+int CaculateValue(int x, int y)
+{
+    int t = 0;
+    t = x * 256;
+    t = t + y;
+    return t;
+}
+
+void value_log()
+{
+    Serial.print("humidity:");
+    Serial.println(humidity);
+    Serial.print("humidity_value:");
+    Serial.println(humidity_value);
+    Serial.print("tem_value:");
+    Serial.println(tem_value);
+
+#ifdef SENSOR_5_PIN
+    Serial.print("ph_value:");
+    Serial.println(ph_value);
+
+    Serial.print("N= ");
+    Serial.print(N_value);
+    Serial.println(" mg/kg");
+    Serial.print("P= ");
+    Serial.print(P_value);
+    Serial.println(" mg/kg");
+    Serial.print("K= ");
+    Serial.print(K_value);
+    Serial.println(" mg/kg");
+
+#endif
+}
